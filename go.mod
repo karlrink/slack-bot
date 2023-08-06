8243cd373cf9b7e3e9b310d85f33efa9726f8fd4
Karl Rink <karl@rink.us>
2023-08-05 20:31:03 -0700

    🐕 2023-08-05

---
+++ go.mod
@@ -0,0 +1,7 @@
+module vwell-bot
+
+go 1.18
+
+require github.com/slack-go/slack v0.12.2
+
+require github.com/gorilla/websocket v1.4.2 // indirect
---
+++ main.go
@@ -0,0 +1,338 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+    "time"
+    "net/http"
+    "io/ioutil"
+    "encoding/json"
+
+	"github.com/slack-go/slack/slackevents"
+	"github.com/slack-go/slack/socketmode"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	appToken := os.Getenv("SLACK_APP_TOKEN")
+	if appToken == "" {
+		panic("SLACK_APP_TOKEN must be set.\n")
+	}
+
+	if !strings.HasPrefix(appToken, "xapp-") {
+		panic("SLACK_APP_TOKEN must have the prefix \"xapp-\".")
+	}
+
+	botToken := os.Getenv("SLACK_BOT_TOKEN")
+	if botToken == "" {
+		panic("SLACK_BOT_TOKEN must be set.\n")
+	}
+
+	if !strings.HasPrefix(botToken, "xoxb-") {
+		panic("SLACK_BOT_TOKEN must have the prefix \"xoxb-\".")
+	}
+
+	api := slack.New(
+		botToken,
+		slack.OptionDebug(true),
+		slack.OptionLog(log.New(os.Stdout, "api: ", log.Lshortfile|log.LstdFlags)),
+		slack.OptionAppLevelToken(appToken),
+	)
+
+	client := socketmode.New(
+		api,
+		socketmode.OptionDebug(true),
+		socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)),
+	)
+
+	socketmodeHandler := socketmode.NewSocketmodeHandler(client)
+
+	socketmodeHandler.Handle(socketmode.EventTypeConnecting, middlewareConnecting)
+	socketmodeHandler.Handle(socketmode.EventTypeConnectionError, middlewareConnectionError)
+	socketmodeHandler.Handle(socketmode.EventTypeConnected, middlewareConnected)
+
+	//\\ EventTypeEventsAPI //\\
+	// Handle all EventsAPI
+	socketmodeHandler.Handle(socketmode.EventTypeEventsAPI, middlewareEventsAPI)
+
+	// Handle a specific event from EventsAPI
+	socketmodeHandler.HandleEvents(slackevents.AppMention, middlewareAppMentionEvent)
+
+	//\\ EventTypeInteractive //\\
+	// Handle all Interactive Events
+	socketmodeHandler.Handle(socketmode.EventTypeInteractive, middlewareInteractive)
+
+	// Handle a specific Interaction
+	socketmodeHandler.HandleInteraction(slack.InteractionTypeBlockActions, middlewareInteractionTypeBlockActions)
+
+	// Handle all SlashCommand
+	socketmodeHandler.Handle(socketmode.EventTypeSlashCommand, middlewareSlashCommand)
+	//socketmodeHandler.HandleSlashCommand("/rocket", middlewareSlashCommand)
+
+	// socketmodeHandler.HandleDefault(middlewareDefault)
+
+	socketmodeHandler.RunEventLoop()
+}
+
+func middlewareConnecting(evt *socketmode.Event, client *socketmode.Client) {
+	fmt.Println("Connecting... to Slack with Socket Mode...")
+}
+
+func middlewareConnectionError(evt *socketmode.Event, client *socketmode.Client) {
+	fmt.Println("Connection failed. Retrying later...")
+}
+
+func middlewareConnected(evt *socketmode.Event, client *socketmode.Client) {
+	fmt.Println("Connected to Slack with Socket Mode.")
+}
+
+
+// Declare a map to keep track of messages that have been responded to
+var respondedMessages = make(map[string]bool)
+
+func middlewareEventsAPI(evt *socketmode.Event, client *socketmode.Client) {
+    fmt.Println("middlewareEventsAPI")
+    eventsAPIEvent, ok := evt.Data.(slackevents.EventsAPIEvent)
+    if !ok {
+        fmt.Printf("Ignored %+v\n", evt)
+        return
+    }
+
+    fmt.Printf("Event received middlewareEventsAPI: %+v\n", eventsAPIEvent)
+
+    client.Ack(*evt.Request)
+
+    switch eventsAPIEvent.Type {
+    case slackevents.CallbackEvent:
+        innerEvent := eventsAPIEvent.InnerEvent
+        switch ev := innerEvent.Data.(type) {
+
+        case *slackevents.MessageEvent:
+            if ev.ChannelType == "im" && ev.BotID == "" {
+                fmt.Printf("Direct message in %v", ev.Channel)
+
+                // Check if we have already responded to this message
+                if _, exists := respondedMessages[ev.ClientMsgID]; !exists {
+                    originalMessage := ev.Text
+                    response := fmt.Sprintf("Howdy, got your message: %s", originalMessage)
+                    _, _, err := client.Client.PostMessage(ev.Channel, slack.MsgOptionText(response, false))
+                    if err != nil {
+                        fmt.Printf("failed posting message: %v", err)
+                    }
+                    // Mark the message as responded in the map
+                    respondedMessages[ev.ClientMsgID] = true
+                }
+            }
+
+        case *slackevents.AppMentionEvent:
+            fmt.Printf("We have been mentioned in %v", ev.Channel)
+            _, _, err := client.Client.PostMessage(ev.Channel, slack.MsgOptionText("Yes, mentioned, ", false))
+            if err != nil {
+                fmt.Printf("failed posting message: %v", err)
+            }
+
+        case *slackevents.MemberJoinedChannelEvent:
+            fmt.Printf("user %q joined to channel %q", ev.User, ev.Channel)
+        }
+
+    default:
+        client.Debugf("unsupported Events API event received")
+    }
+}
+
+
+func middlewareAppMentionEvent(evt *socketmode.Event, client *socketmode.Client) {
+	fmt.Println("middlewareAppMentionEvent")
+	eventsAPIEvent, ok := evt.Data.(slackevents.EventsAPIEvent)
+	if !ok {
+		fmt.Printf("Ignored %+v\n", evt)
+		return
+	}
+
+	fmt.Printf("EventMention received middlewareAppMentionEvent: %+v\n", eventsAPIEvent)
+
+	client.Ack(*evt.Request)
+
+	ev, ok := eventsAPIEvent.InnerEvent.Data.(*slackevents.AppMentionEvent)
+	if !ok {
+		fmt.Printf("Ignored %+v\n", ev)
+		return
+	}
+
+	fmt.Printf("We have been mentioned in %v\n", ev.Channel)
+	_, _, err := client.Client.PostMessage(ev.Channel, slack.MsgOptionText("Oh, hello.", false))
+	if err != nil {
+		fmt.Printf("failed posting message: %v", err)
+	}
+}
+
+func middlewareInteractive(evt *socketmode.Event, client *socketmode.Client) {
+	callback, ok := evt.Data.(slack.InteractionCallback)
+	if !ok {
+		fmt.Printf("Ignored %+v\n", evt)
+		return
+	}
+
+	fmt.Printf("Interaction received: %+v\n", callback)
+
+	var payload interface{}
+
+	switch callback.Type {
+	case slack.InteractionTypeBlockActions:
+		// See https://api.slack.com/apis/connections/socket-implement#button
+		client.Debugf("button clicked!")
+	case slack.InteractionTypeShortcut:
+	case slack.InteractionTypeViewSubmission:
+		// See https://api.slack.com/apis/connections/socket-implement#modal
+	case slack.InteractionTypeDialogSubmission:
+	default:
+
+	}
+
+	client.Ack(*evt.Request, payload)
+}
+
+func middlewareInteractionTypeBlockActions(evt *socketmode.Event, client *socketmode.Client) {
+	client.Debugf("button clicked!")
+}
+
+func middlewareSlashCommand(evt *socketmode.Event, client *socketmode.Client) {
+	cmd, ok := evt.Data.(slack.SlashCommand)
+	if !ok {
+		fmt.Printf("Ignored %+v\n", evt)
+		return
+	}
+
+	client.Debugf("Slash command received: %+v", cmd)
+
+    switch cmd.Command {
+    case "/dadjoke":
+        handleDadJokeCommand(evt, client)
+    case "/weather":
+        handleWeatherCommand(evt, client)
+    default:
+        // If the command is not one of the specified commands, ignore and return
+        fmt.Printf("Ignored %+v\n", evt)
+        return
+    }
+
+}
+
+
+func handleDadJokeCommand(evt *socketmode.Event, client *socketmode.Client) {
+    client.Debugf("Slash command '/dadjoke' received: %+v", evt)
+
+    // Add your response logic for the "/dadjoke" command here
+
+    // Example response with a Dad joke
+    //responseText := "Why don't scientists trust atoms? Because they make up everything! 😄"
+
+    jokeText, err := getDadJoke()
+    if err != nil {
+        jokeText = "Not a Joke! " + err.Error()
+    }
+
+    payload := map[string]interface{}{
+        "blocks": []slack.Block{
+            slack.NewSectionBlock(
+                &slack.TextBlockObject{
+                    Type: slack.MarkdownType,
+                    Text: jokeText, //foo
+                },
+                nil,
+                slack.NewAccessory(
+                    slack.NewButtonBlockElement(
+                        "",
+                        "somevalue",
+                        &slack.TextBlockObject{
+                            Type: slack.PlainTextType,
+                            Text: "bar",
+                        },
+                    ),
+                ),
+            ),
+        },
+    }
+
+    client.Ack(*evt.Request, payload)
+}
+
+type JokeResponse struct {
+	Joke string `json:"joke"`
+}
+
+func getDadJoke() (string, error) {
+	url := "https://icanhazdadjoke.com/"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Accept", "application/json")
+
+	client := &http.Client{Timeout: 5 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var jokeResp JokeResponse
+	err = json.Unmarshal(body, &jokeResp)
+	if err != nil {
+		return "", err
+	}
+
+	return jokeResp.Joke, nil
+}
+
+func handleWeatherCommand(evt *socketmode.Event, client *socketmode.Client) {
+    client.Debugf("Slash command '/weather' received: %+v", evt)
+
+    // Add your response logic for the "/weather" command here
+
+    // Example response 
+    responseText := "102 °F Temperatures are on the up!  the water is warm."
+
+    payload := map[string]interface{}{
+        "blocks": []slack.Block{
+            slack.NewSectionBlock(
+                &slack.TextBlockObject{
+                    Type: slack.MarkdownType,
+                    Text: responseText,
+                },
+                nil,
+                slack.NewAccessory(
+                    slack.NewButtonBlockElement(
+                        "",
+                        "somevalue",
+                        &slack.TextBlockObject{
+                            Type: slack.PlainTextType,
+                            Text: "wet bar",
+                        },
+                    ),
+                ),
+            ),
+        },
+    }
+
+    client.Ack(*evt.Request, payload)
+}
+
+
+
+
+
+func middlewareDefault(evt *socketmode.Event, client *socketmode.Client) {
+	// fmt.Fprintf(os.Stderr, "Unexpected event type received: %s\n", evt.Type)
+}
+
+
