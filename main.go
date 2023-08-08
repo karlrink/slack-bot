5e977fcc211614225c273f95dac78cbbe019d548
Karl Rink <karl@rink.us>
2023-08-07 18:44:00 -0700

    🐕 2023-08-07 🐕 🐕 🐕

--- main.go
+++ main.go
@@ -130,10 +130,10 @@ func middlewareEventsAPI(evt *socketmode.Event, client *socketmode.Client) {
 
                     originalMessage := strings.ToLower(ev.Text) // Convert to lowercase
 
-                    if strings.HasPrefix(originalMessage, "tell a dad joke in channel") {
+                    if strings.HasPrefix(originalMessage, "Tell a dad joke in channel") {
 
                         //channelID := strings.TrimPrefix(originalMessage, "tell a dad joke in channel <#")
-                        channelID := strings.TrimPrefix(ev.Text, "tell a dad joke in channel <#")
+                        channelID := strings.TrimPrefix(ev.Text, "Tell a dad joke in channel <#")
                         channelID = strings.Split(channelID, "|")[0] // Assuming the channel mention format is <#CHANNEL_ID|name>
 
                         jokeText, jokeErr := getDadJoke()
